1f0a79fbcb45dfbdc4b11a19cb6fc499039bbe84
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-12-27 20:52:18 +0200

    Fixed an error in the algorithm

--- src/main/java/algorithm/DijkstrasAlgorithm.java
+++ src/main/java/algorithm/DijkstrasAlgorithm.java
@@ -1,18 +1,17 @@
 package algorithm;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Stream;
+
+import static java.util.stream.Stream.iterate;
 
 public class DijkstrasAlgorithm<T> implements SearchAlgorithm<T> {
 
     @Override
     public List<T> findPath(Graph<T> graph, T source, T target) {
         final var queue = new LinkedList<T>();
-        final var visited = new HashSet<T>();
         final var distances = new HashMap<T, Double>();
         final var previous = new HashMap<T, T>();
         queue.add(source);
@@ -23,19 +22,16 @@ public class DijkstrasAlgorithm<T> implements SearchAlgorithm<T> {
             final var edges = graph.nodes().get(prev);
             edges.forEach((node, time) -> {
                 final var distance = distances.get(prev) + time.doubleValue();
-                if (!visited.contains(node)) {
-                    queue.addLast(node);
-                    visited.add(node);
-                }
                 if (distance < distances.getOrDefault(node, Double.MAX_VALUE)) {
                     previous.put(node, prev);
                     distances.put(node, distance);
+                    queue.addLast(node);
                 }
             });
         }
 
         final var path = new LinkedList<T>();
-        Stream.iterate(target, Objects::nonNull, previous::get).forEach(path::addFirst);
+        iterate(target, Objects::nonNull, previous::get).forEach(path::addFirst);
         return path;
     }
 
