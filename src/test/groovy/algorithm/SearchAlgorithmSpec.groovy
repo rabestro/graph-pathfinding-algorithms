07b71f5a7dabf90d1973092b774b3cf705516c63
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-12-27 21:33:42 +0200

    Add test report

--- src/test/groovy/algorithm/SearchAlgorithmSpec.groovy
+++ src/test/groovy/algorithm/SearchAlgorithmSpec.groovy
@@ -1,19 +1,21 @@
 package algorithm
 
+import spock.lang.Issue
 import spock.lang.Specification
 import spock.lang.Subject
-import spock.lang.Unroll
+import spock.lang.Title
 
+@Title("Comparison of two algorithms")
 class SearchAlgorithmSpec extends Specification {
     @Subject
-    def shortest = new BreadthFirstSearch<String>()
+    def bfsAlgorithm = new BreadthFirstSearch<String>()
 
     @Subject
-    def fastest = new DijkstrasAlgorithm<String>()
+    def dijkstras = new DijkstrasAlgorithm<String>()
 
-    @Unroll("from #source to #target the time is #time and the fastest path is #fastestPath")
+    @Issue("https://github.com/rabestro/graph-search-algorithm-java/blob/master/docs/complex.gif")
     def 'should find a route for a complex graph'() {
-        given:
+        given: 'A complex graph sample'
         def graph = new Graph<String>([
                 A: [B: 5, H: 2],
                 B: [A: 5, C: 7],
@@ -25,32 +27,36 @@ class SearchAlgorithmSpec extends Specification {
                 H: [G: 3]
         ])
 
-        when:
-        def routeOne = shortest.findPath(graph, source, target)
+        when: 'we use Breadth First Search algorithm for the first route'
+        def routeOne = bfsAlgorithm.findPath(graph, source, target)
 
-        and:
-        def routeTwo = fastest.findPath(graph, source, target)
+        and: 'we use Dijkstras algorithm for the second route'
+        def routeTwo = dijkstras.findPath(graph, source, target)
 
-        then:
-        routeOne == shortestPath
+        then: "the first route is the shortest"
+        routeOne == shortest
 
-        and:
-        routeTwo == fastestPath
+        and: 'the second route is the fastest'
+        routeTwo == fastest
+
+        and: 'the distance calculated correctly'
+        graph.getDistance(routeOne) == d1 as double
+        graph.getDistance(routeTwo) == d2 as double
 
         where:
-        source | target || time | shortestPath              | fastestPath
-        'A'    | 'A'    || 0    | ['A']                     | ['A']
-        'B'    | 'B'    || 0    | ['B']                     | ['B']
-        'A'    | 'B'    || 5    | ['A', 'B']                | ['A', 'B']
-        'B'    | 'A'    || 5    | ['B', 'A']                | ['B', 'A']
-        'A'    | 'C'    || 9    | ['A', 'B', 'C']           | ['A', 'H', 'G', 'C']
-        'C'    | 'A'    || 12   | ['C', 'B', 'A']           | ['C', 'B', 'A']
-        'A'    | 'G'    || 5    | ['A', 'H', 'G']           | ['A', 'H', 'G']
-        'C'    | 'D'    || 3    | ['C', 'D']                | ['C', 'D']
-        'D'    | 'C'    || 19   | ['D', 'C']                | ['D', 'E', 'F', 'G', 'C']
-        'B'    | 'D'    || 10   | ['B', 'C', 'D']           | ['B', 'C', 'D']
-        'D'    | 'B'    || 26   | ['D', 'C', 'B']           | ['D', 'E', 'F', 'G', 'C', 'B']
-        'D'    | 'H'    || 33   | ['D', 'C', 'B', 'A', 'H'] | ['D', 'E', 'F', 'G', 'C', 'B', 'A', 'H']
+        source | target || d1 | shortest                  | d2 | fastest
+        'A'    | 'A'    || 0  | ['A']                     | 0  | ['A']
+        'B'    | 'B'    || 0  | ['B']                     | 0  | ['B']
+        'A'    | 'B'    || 5  | ['A', 'B']                | 5  | ['A', 'B']
+        'B'    | 'A'    || 5  | ['B', 'A']                | 5  | ['B', 'A']
+        'A'    | 'C'    || 12 | ['A', 'B', 'C']           | 9  | ['A', 'H', 'G', 'C']
+        'C'    | 'A'    || 12 | ['C', 'B', 'A']           | 12 | ['C', 'B', 'A']
+        'A'    | 'G'    || 5  | ['A', 'H', 'G']           | 5  | ['A', 'H', 'G']
+        'C'    | 'D'    || 3  | ['C', 'D']                | 3  | ['C', 'D']
+        'D'    | 'C'    || 20 | ['D', 'C']                | 19 | ['D', 'E', 'F', 'G', 'C']
+        'B'    | 'D'    || 10 | ['B', 'C', 'D']           | 10 | ['B', 'C', 'D']
+        'D'    | 'B'    || 27 | ['D', 'C', 'B']           | 26 | ['D', 'E', 'F', 'G', 'C', 'B']
+        'D'    | 'H'    || 34 | ['D', 'C', 'B', 'A', 'H'] | 33 | ['D', 'E', 'F', 'G', 'C', 'B', 'A', 'H']
     }
 
 }
