107e06ff4c243be8aa5dd0f25997170e1303c3d2
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-01-03 11:00:59 +0200

    Update DijkstrasAlgorithm.java

--- src/main/java/lv/id/jc/algorithm/graph/DijkstrasAlgorithm.java
+++ src/main/java/lv/id/jc/algorithm/graph/DijkstrasAlgorithm.java
@@ -19,17 +19,17 @@ import static java.util.stream.Stream.iterate;
 public class DijkstrasAlgorithm<T> implements SearchAlgorithm<T> {
 
     @Override
-    public List<T> findPath(final Graph<T> graph, T source, T target) {
-        final var queue = new LinkedList<T>();
-        final var distances = new HashMap<T, Double>();
-        final var previous = new HashMap<T, T>();
+    public List<T> findPath(Graph<T> graph, T source, T target) {
+        var queue = new LinkedList<T>();
+        var distances = new HashMap<T, Double>();
+        var previous = new HashMap<T, T>();
         queue.add(source);
         distances.put(source, .0);
 
         while (!queue.isEmpty()) {
-            final var prev = queue.removeFirst();
+            var prev = queue.removeFirst();
             graph.edges(prev).forEach((node, time) -> {
-                final var distance = distances.get(prev) + time.doubleValue();
+                var distance = distances.get(prev) + time.doubleValue();
                 if (distance < distances.getOrDefault(node, Double.MAX_VALUE)) {
                     previous.put(node, prev);
                     distances.put(node, distance);
@@ -38,7 +38,7 @@ public class DijkstrasAlgorithm<T> implements SearchAlgorithm<T> {
             });
         }
         if (previous.containsKey(target) || source.equals(target)) {
-            final var path = new LinkedList<T>();
+            var path = new LinkedList<T>();
             iterate(target, Objects::nonNull, previous::get).forEach(path::addFirst);
             return path;
         }
