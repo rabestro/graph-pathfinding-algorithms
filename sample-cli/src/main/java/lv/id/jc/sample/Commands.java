5e7c2b675a92e3d6a57e1e4a750d7873b537851f
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-02-09 14:32:40 +0200

    Updated settings.gradle

--- sample-cli/src/main/java/lv/id/jc/sample/Commands.java
+++ sample-cli/src/main/java/lv/id/jc/sample/Commands.java
@@ -2,6 +2,7 @@ package lv.id.jc.sample;
 
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import lv.id.jc.algorithm.graph.BreadthFirstSearch;
+import lv.id.jc.algorithm.graph.DijkstrasAlgorithm;
 import lv.id.jc.algorithm.graph.Graph;
 import lv.id.jc.algorithm.graph.SearchAlgorithm;
 import org.jline.utils.AttributedString;
@@ -17,18 +18,24 @@ import java.util.Map;
 
 @ShellComponent
 public class Commands implements PromptProvider, InitializingBean {
+    private final SearchAlgorithm<String> bfgAlgorithm = new BreadthFirstSearch<>();
+    private final SearchAlgorithm<String> dijkstrasAlgorithm = new DijkstrasAlgorithm<>();
 
-    @Value("${graph.file:complex}")
-    private String file;
+    @Value("${graph:complex}")
+    private String graphName;
 
     private Graph<String> graph;
-    private SearchAlgorithm<String> bfgAlgorithm = new BreadthFirstSearch<>();
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        var in = Commands.class.getClassLoader().getResourceAsStream(file + ".yaml");
+        var in = Commands.class.getClassLoader().getResourceAsStream(graphName + ".yaml");
         var schema = new YAMLMapper().readValue(in, Map.class);
         graph = Graph.of(schema);
+    }
+
+    @Override
+    public AttributedString getPrompt() {
+        return new AttributedString(graphName + ":> ", AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW));
     }
 
     @ShellMethod("finds the shortest path by using Breadth First Search Algorithm")
@@ -36,8 +43,18 @@ public class Commands implements PromptProvider, InitializingBean {
         return bfgAlgorithm.findPath(graph, source, target);
     }
 
-    @Override
-    public AttributedString getPrompt() {
-        return new AttributedString(file + ":>", AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW));
+    @ShellMethod("finds the fastest path by using Dijkstra's Algorithm")
+    public List<String> fastest(String source, String target) {
+        return dijkstrasAlgorithm.findPath(graph, source, target);
+    }
+
+    @ShellMethod("prints schema of the graph")
+    public Map<String, Map<String, Number>> schema() {
+        return graph.schema();
+    }
+
+    @ShellMethod("prints distance for the path")
+    public double distance(List<String> path) {
+        return graph.getDistance(path);
     }
 }
